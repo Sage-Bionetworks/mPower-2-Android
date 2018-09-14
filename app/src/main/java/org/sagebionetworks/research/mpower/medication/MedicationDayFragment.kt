bdd2ee12a92a03646bb1f8ccd2864896040d745a
Joshua Liu <liujoshua@users.noreply.github.com>
2018-09-13 18:35:50 -0700

    instance state for MedicationDayFragment; Update BridgeAndroidSDK

--- app/src/main/java/org/sagebionetworks/research/mpower/medication/MedicationDayFragment.kt
+++ app/src/main/java/org/sagebionetworks/research/mpower/medication/MedicationDayFragment.kt
@@ -1,9 +1,8 @@
 package org.sagebionetworks.research.mpower.medication
 
-import android.app.Activity
 import android.app.AlertDialog
-import android.content.Context
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatDialogFragment
 import android.support.v7.widget.DividerItemDecoration
@@ -22,11 +21,10 @@ import org.sagebionetworks.research.mpower.R.layout
 import org.slf4j.LoggerFactory
 import java.util.Arrays
 
-class MedicationDayFragment: AppCompatDialogFragment() {
+class MedicationDayFragment : AppCompatDialogFragment() {
 
     private val LOGGER = LoggerFactory.getLogger(
             MedicationDayFragment::class.java)
-
 
     companion object {
         val ARG_SCHED_ID = "ARG_SCHED_ID"
@@ -55,30 +53,34 @@ class MedicationDayFragment: AppCompatDialogFragment() {
     lateinit var time: String
     lateinit var days: String
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if(getArguments() != null) {
-            schedId = arguments!!.getString(ARG_SCHED_ID)
-            name = arguments!!.getString(ARG_NAME)
-            time = arguments!!.getString(ARG_TIME)
-            days = arguments!!.getString(ARG_DAYS)
+        if (savedInstanceState == null) {
+            if (arguments != null) {
+                schedId = arguments!!.getString(ARG_SCHED_ID)
+                name = arguments!!.getString(ARG_NAME)
+                time = arguments!!.getString(ARG_TIME)
+                days = arguments!!.getString(ARG_DAYS)
 
-            selectedDays = days.split(",").toMutableList()
-            val size = selectedDays.size
-            LOGGER.debug("Selected days on init: $size")
+                selectedDays = days.split(",").toMutableList()
+                val size = selectedDays.size
+                LOGGER.debug("Selected days on init: $size")
+            } else {
+                LOGGER.warn("No arguments found")
+                name = "Default"
+                time = "9:15 PM"
+            }
         } else {
-            LOGGER.warn("No arguments found")
-            name = "Default"
-            time = "9:15 PM"
+            schedId = savedInstanceState.getString(ARG_SCHED_ID)
+            name = savedInstanceState.getString(ARG_NAME)
+            time = savedInstanceState.getString(ARG_TIME)
+            days = savedInstanceState.getString(ARG_DAYS)
         }
-
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?): View?
-    {
+            savedInstanceState: Bundle?): View? {
         LOGGER.debug("onCreateView()")
         return customView
     }
@@ -104,33 +106,40 @@ class MedicationDayFragment: AppCompatDialogFragment() {
         recycler.adapter = DayAdapter(getDays(), context!!)
         recycler.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
 
-        day_selection_back.setOnClickListener {
-            _ -> dismiss()
+        day_selection_back.setOnClickListener { _ ->
+            dismiss()
         }
 
-        day_selection_save.setOnClickListener {
-            _ ->
-                listener.onDaySelected(schedId, selectedDays.joinToString(","))
-                dismiss()
+        day_selection_save.setOnClickListener { _ ->
+            listener.onDaySelected(schedId, selectedDays.joinToString(","))
+            dismiss()
         }
-
-
     }
 
-    override fun onAttach(activity: Activity) {
-        super.onAttach(activity)
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
         try {
-            listener = activity as DaySelectedListener
+            listener = context as DaySelectedListener
         } catch (e: ClassCastException) {
             throw ClassCastException(activity.toString() + " must implement DaySelectedListener")
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString(ARG_SCHED_ID, schedId)
+        outState.putString(ARG_NAME, name)
+        outState.putString(ARG_TIME, time)
+        outState.putString(ARG_DAYS, days)
     }
 
     fun getDays(): ArrayList<String> {
         return ArrayList<String>(Arrays.asList(*resources.getStringArray(R.array.days_of_the_week)))
     }
 
-    inner class DayAdapter(val items : ArrayList<String>, val context: Context) : RecyclerView.Adapter<DayViewHolder>() {
+    inner class DayAdapter(val items: ArrayList<String>, val context: Context) :
+            RecyclerView.Adapter<DayViewHolder>() {
 
         override fun getItemCount(): Int {
             return items.size
@@ -144,21 +153,20 @@ class MedicationDayFragment: AppCompatDialogFragment() {
         override fun onBindViewHolder(holder: DayViewHolder, position: Int) {
             var text = items.get(position)
             holder.tvDay.text = text
-            if(selectedDays.contains(text)) {
+            if (selectedDays.contains(text)) {
                 holder.root.setBackgroundResource(R.color.royal300)
             } else {
                 holder.root.setBackgroundResource(R.color.appWhite)
             }
 
-            holder.root.setOnClickListener {
-                view ->
-                    if(selectedDays.contains(text)) {
-                        selectedDays.remove(text)
-                        view.setBackgroundResource(R.color.appWhite)
-                    } else {
-                        selectedDays.add(text)
-                        view.setBackgroundResource(R.color.royal300)
-                    }
+            holder.root.setOnClickListener { view ->
+                if (selectedDays.contains(text)) {
+                    selectedDays.remove(text)
+                    view.setBackgroundResource(R.color.appWhite)
+                } else {
+                    selectedDays.add(text)
+                    view.setBackgroundResource(R.color.royal300)
+                }
             }
         }
     }
@@ -168,8 +176,7 @@ interface DaySelectedListener {
     fun onDaySelected(id: String, days: String)
 }
 
-
-class DayViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+class DayViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     var root = view
     val tvDay = view.day_text
 }
