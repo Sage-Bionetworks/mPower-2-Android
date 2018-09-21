b1209ab3697a16ebfa263be27b97c190ed70aee1
Khai Do <zaro0508@gmail.com>
2018-09-21 13:04:22 -0700

    Fixup build files
    * Remove html files
    * add firebase dependencies to gradle files
    * add travis stage for firebase testing
    * move deploy command to .travis.yml file

--- travis/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-bundle exec fastlane internal alias:"$KEY_ALIAS" storepass:"$KEYSTORE_PASSWORD" keypass:"$KEY_PASSWORD" signed_apk_path:"app/build/outputs/apk/app-internal-release.apk"
-
