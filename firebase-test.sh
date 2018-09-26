71e14c0b75f215ce0f91474c8232d753c5751e6e
Khai Do <zaro0508@gmail.com>
2018-09-26 13:47:07 -0700

    update gcloud service account for firebase

--- firebase-test.sh
+++ firebase-test.sh
@@ -4,5 +4,5 @@ set -e
 gcloud config set project mpower2-e03b9
 gcloud --quiet components update
 gcloud --quiet components install beta
-gcloud auth activate-service-account travis@mpower2-215017.iam.gserviceaccount.com --key-file ../android-certificates/gcloud_service_account_6f4ab81e.json
+gcloud auth activate-service-account mpower2-e03b9@appspot.gserviceaccount.com --key-file ../android-certificates/gcloud_service_account_106c1870.json
 echo "y" | gcloud firebase test android run --app ./app/build/outputs/apk/internal/debug/app-internal-debug.apk --test ./app/build/outputs/apk/androidTest/internal/debug/app-internal-debug-androidTest.apk
