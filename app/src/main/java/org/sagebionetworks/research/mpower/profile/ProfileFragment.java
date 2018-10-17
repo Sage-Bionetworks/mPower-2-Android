c1ef555eaef89b278d312a6fc2f1eaf5ac52d3e1
Aishwarya Mandyam <aishwarya_m@live.com>
2018-10-17 09:40:16 -0700

    Removing class path and updating title string

--- app/src/main/java/org/sagebionetworks/research/mpower/profile/ProfileFragment.java
+++ app/src/main/java/org/sagebionetworks/research/mpower/profile/ProfileFragment.java
@@ -2,6 +2,7 @@ package org.sagebionetworks.research.mpower.profile;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -66,8 +67,8 @@ public class ProfileFragment extends Fragment {
     public void onLicensesClicked() {
         LOGGER.debug("Clicked on the licenses button");
         String path =  MpResourceManager.getInstance().getLicense().getAbsolutePath();
-        android.content.Intent intent = org.researchstack.backbone.ui.ViewWebDocumentActivity.newIntentForPath(this.getActivity(),
-                getString(org.researchstack.backbone.R.string.rsb_settings_privacy_policy), path);
+        Intent intent = org.researchstack.backbone.ui.ViewWebDocumentActivity.newIntentForPath(this.getActivity(),
+                "", path);
         startActivity(intent);
 
 
