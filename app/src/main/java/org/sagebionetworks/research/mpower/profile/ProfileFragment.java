a0b93e1456e798291ce6544819de67ed05e5f5e4
Aishwarya Mandyam <aishwarya_m@live.com>
2018-10-17 12:13:03 -0700

    Updating the build version automatically

--- app/src/main/java/org/sagebionetworks/research/mpower/profile/ProfileFragment.java
+++ app/src/main/java/org/sagebionetworks/research/mpower/profile/ProfileFragment.java
@@ -3,6 +3,8 @@ package org.sagebionetworks.research.mpower.profile;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,12 +12,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import org.researchstack.backbone.ResourceManager;
 import org.sagebionetworks.research.mpower.R;
 import org.researchstack.backbone.ResourcePathManager;
 
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
@@ -24,6 +28,7 @@ import dagger.android.support.AndroidSupportInjection;
 import org.sagebionetworks.research.mpower.researchstack.framework.MpResourceManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.researchstack.backbone.utils.LogExt;
 
 public class ProfileFragment extends Fragment {
 
@@ -32,6 +37,8 @@ public class ProfileFragment extends Fragment {
     private Unbinder unbinder;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ProfileFragment.class);
+
+    @BindView(R.id.footer_text) TextView footerTextView;
 
 
     @Override
@@ -72,6 +79,30 @@ public class ProfileFragment extends Fragment {
         startActivity(intent);
 
 
+        String current_version = getVersionString();
+        footerTextView.setText(current_version);
+
+    }
+
+    public String getVersionString()
+    {
+        int versionCode;
+        String versionName;
+        PackageManager manager = getActivity().getPackageManager();
+
+        try
+        {
+            PackageInfo info = manager.getPackageInfo(getActivity().getPackageName(), 0);
+            versionCode = info.versionCode;
+            versionName = info.versionName;
+        }
+        catch(PackageManager.NameNotFoundException e)
+        {
+            LogExt.e(getClass(), "Could not find package version info");
+            versionCode = 0;
+            versionName = getString(R.string.rsb_settings_version_unknown);
+        }
+        return getString(R.string.rsb_settings_version, versionName, versionCode);
     }
 
 }
