f726bf79af26ace40c1f893b9de222adef563cf8
MDP <mike@sdpdigital.com>
2018-10-27 15:30:58 -0400

    Added support for tab save/load and study burst schedule references

--- app/src/main/java/org/sagebionetworks/research/mpower/tracking/TrackingMenuFragment.java
+++ app/src/main/java/org/sagebionetworks/research/mpower/tracking/TrackingMenuFragment.java
@@ -2,7 +2,14 @@ package org.sagebionetworks.research.mpower.tracking;
 
 import static com.google.common.base.Preconditions.checkState;
 
-import android.content.Intent;
+import static org.sagebionetworks.research.mpower.research.MpIdentifier.TAPPING;
+import static org.sagebionetworks.research.mpower.research.MpIdentifier.TREMOR;
+import static org.sagebionetworks.research.mpower.research.MpIdentifier.WALK_AND_BALANCE;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.ColorRes;
@@ -14,20 +21,23 @@ import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.sagebionetworks.research.mpower.R;
 import org.sagebionetworks.research.mpower.TaskLauncher;
-import org.sagebionetworks.research.mpower.tracking.fragment.MedicationSchedulingFragment;
 import org.sagebionetworks.research.mpower.Tasks;
+import org.sagebionetworks.research.mpower.viewmodel.StudyBurstItem;
+import org.sagebionetworks.research.mpower.viewmodel.StudyBurstTaskInfo;
+import org.sagebionetworks.research.mpower.viewmodel.StudyBurstViewModel;
+import org.sagebionetworks.research.sageresearch.manager.TaskInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 import javax.inject.Inject;
 
@@ -77,7 +87,6 @@ public class TrackingMenuFragment extends Fragment {
     private static final int SELECTED_COLOR = R.color.royal500;
     @ColorRes
     private static final int UNSELECTED_COLOR = R.color.royal400;
-    private int selectedId;
     private boolean showingContent;
 
     @BindView(R.id.tracking_tab)
@@ -101,32 +110,43 @@ public class TrackingMenuFragment extends Fragment {
     @BindViews({R.id.centerIconImageView, R.id.leftIconImageView, R.id.rightIconImageView})
     protected List<ImageView> imageViews;
 
+    @BindViews({R.id.centerIconCheckmark, R.id.leftIconCheckmark, R.id.rightIconCheckmark})
+    protected List<ImageView> checkMarkImageViews;
+
     private Unbinder unbinder;
     private GestureDetectorCompat gestureDetector;
+
+    @Inject
+    StudyBurstViewModel.Factory studyBurstViewModelFactory;
+    private @Nullable StudyBurstViewModel studyBurstViewModel;
+
+    private @NonNull TrackingMenuFragmentViewModel trackingMenuViewModel;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AndroidSupportInjection.inject(this);
+
+        trackingMenuViewModel =
+                ViewModelProviders.of(this).get(TrackingMenuFragmentViewModel.class);
+        studyBurstViewModel = ViewModelProviders.of(this,
+                studyBurstViewModelFactory).get(StudyBurstViewModel.class);
+        studyBurstViewModel.liveData().observe(this, this::setupMeasuringTaskCompletionState);
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup group, @Nullable Bundle savedInstanceState) {
         View result = inflater.inflate(R.layout.tracking_menu_fragment, group, false);
         this.unbinder = ButterKnife.bind(this, result);
-        this.updateMenuState(true, false);
-        this.setIcons(TRACKING_ICONS, TRACKING_LABELS);
-        this.selectedId = this.trackingButton.getId();
-        this.trackingButton.setOnClickListener(view -> updateSelection(R.id.tracking_tab));
-        this.measuringButton.setOnClickListener(view -> updateSelection(R.id.measuring_tab));
+        updateSelection(trackingMenuViewModel.getCurrentSelectedId(), true);
+        this.trackingButton.setOnClickListener(view -> updateSelection(R.id.tracking_tab, false));
+        this.measuringButton.setOnClickListener(view -> updateSelection(R.id.measuring_tab, false));
         this.setIconListeners();
-        this.showingContent = true;
         this.gestureDetector = new GestureDetectorCompat(this.getContext(), new FlingListener());
         result.setOnTouchListener((view, event) -> {
             view.performClick();
             return this.gestureDetector.onTouchEvent(event);
         });
-
         return result;
     }
 
@@ -134,16 +154,26 @@ public class TrackingMenuFragment extends Fragment {
         for (int i = 0; i < this.imageViews.size(); i++) {
             final int copy = i;
             this.imageViews.get(i).setOnClickListener(view -> {
+
+                if (getContext() == null) {
+                    return;  // Guard NPE exceptions
+                }
+
                 int selection = 0;
-                if (this.selectedId == R.id.tracking_tab) {
+                if (trackingMenuViewModel.getCurrentSelectedId() == R.id.tracking_tab) {
                     selection = TRACKING_LABELS.get(copy);
-                } else if (this.selectedId == R.id.measuring_tab) {
+                } else if (trackingMenuViewModel.getCurrentSelectedId() == R.id.measuring_tab) {
                     selection = MEASURING_LABELS.get(copy);
                 }
 
                 String taskIdentifier = this.getTaskIdentifierFromLabel(selection);
                 if (taskIdentifier != null) {
-                    launcher.launchTask(this.getContext(), taskIdentifier, null);
+                    @Nullable String taskGuid = findScheduleGuidForMeasuringTask(taskIdentifier);
+                    @Nullable UUID uuid = null;
+                    if (studyBurstViewModel != null) {
+                        uuid = studyBurstViewModel.createScheduleTaskRunUuid(taskGuid);
+                    }
+                    launcher.launchTask(getContext(), taskIdentifier, uuid);
                 } else {
                     LOGGER.warn("Selected Icon " + selection + " doesn't map to a task identifier");
                 }
@@ -185,11 +215,9 @@ public class TrackingMenuFragment extends Fragment {
     }
 
     public void setShowingContent(boolean showingContent) {
-        if (this.showingContent != showingContent) {
-            this.showingContent = showingContent;
-            int visibility = showingContent ? View.VISIBLE : View.GONE;
-            this.contentView.setVisibility(visibility);
-        }
+        this.showingContent = showingContent;
+        int visibility = showingContent ? View.VISIBLE : View.GONE;
+        this.contentView.setVisibility(visibility);
     }
 
     @Override
@@ -199,10 +227,11 @@ public class TrackingMenuFragment extends Fragment {
     }
 
 
-    private void updateSelection(int selectionId) {
-        if (selectionId != this.selectedId) {
+    private void updateSelection(int selectionId, boolean forceUpdate) {
+        if (selectionId != 0 &&
+                (forceUpdate || selectionId != trackingMenuViewModel.getCurrentSelectedId())) {
             this.setShowingContent(true);
-            this.selectedId = selectionId;
+            trackingMenuViewModel.setSelectedId(selectionId);
             if (selectionId == R.id.tracking_tab) {
                 this.setIcons(TRACKING_ICONS, TRACKING_LABELS);
                 this.updateMenuState(true, false);
@@ -212,7 +241,7 @@ public class TrackingMenuFragment extends Fragment {
             }
         } else {
             this.setShowingContent(false);
-            this.selectedId = 0;
+            trackingMenuViewModel.setSelectedId(0);
             this.updateMenuState(false, false);
         }
     }
@@ -229,5 +258,80 @@ public class TrackingMenuFragment extends Fragment {
         this.measuringButton.setTextColor(measuringButtonColor);
         this.trackingSelectedImageView.setAlpha(trackingImageViewAlpha);
         this.measuringSelectedImageView.setAlpha(measuringImageViewAlpha);
+        refreshTabCompletionState();
+    }
+
+    /**
+     * @param measuringTaskIdentifier of the schedule guid to find
+     * @return the guid of the measuring task schedule if one can be found, null otheriwse
+     */
+    private @Nullable String findScheduleGuidForMeasuringTask(@NonNull String measuringTaskIdentifier) {
+        if (studyBurstViewModel == null) {
+            return null; // Guard NPE exceptions
+        }
+        StudyBurstItem item = studyBurstViewModel.liveData().getValue();
+        if (item == null) {
+            LOGGER.warn("Measuring task schedule could not be found for task id " + measuringTaskIdentifier);
+            return null;
+        }
+        for (StudyBurstTaskInfo studyBurstTaskInfo : item.getOrderedTasks()) {
+            if (studyBurstTaskInfo.getSchedule() != null &&
+                    measuringTaskIdentifier.equals(studyBurstTaskInfo.getTask().getIdentifier())) {
+                return studyBurstTaskInfo.getSchedule().getGuid();
+            }
+        }
+        LOGGER.warn("Measuring task schedule could not be found for task id " + measuringTaskIdentifier);
+        return null;
+    }
+
+    /**
+     * Refreshes the tab's completion states based on the current value of the live data
+     */
+    private void refreshTabCompletionState() {
+        if (studyBurstViewModel == null) {
+            return; // Guard NPE exceptions
+        }
+        setupMeasuringTaskCompletionState(studyBurstViewModel.liveData().getValue());
+    }
+
+    /**
+     * Sets the completion check mark state of the measuring tasks based on their study burst state
+     * @param item the study burst item that contains the study burst measuring task states
+     */
+    private void setupMeasuringTaskCompletionState(@Nullable StudyBurstItem item) {
+        for (ImageView checkMarkImageView : checkMarkImageViews) {
+            checkMarkImageView.setVisibility(View.GONE);
+        }
+        if (item == null ) {
+            return; // Guard NPE exceptions
+        }
+        if (trackingMenuViewModel.getCurrentSelectedId() != R.id.measuring_tab) {
+            return; // Tracking tasks don't have completion states
+        }
+        for (StudyBurstTaskInfo studyBurstTaskInfo : item.getOrderedTasks()) {
+            if (studyBurstTaskInfo.isComplete()) {
+                TaskInfo taskInfo = studyBurstTaskInfo.getTask();
+                if (WALK_AND_BALANCE.equals(taskInfo.getIdentifier())) {
+                    checkMarkImageViews.get(0).setVisibility(View.VISIBLE);
+                } else if (TAPPING.equals(taskInfo.getIdentifier())) {
+                    checkMarkImageViews.get(1).setVisibility(View.VISIBLE);
+                } else if (TREMOR.equals(taskInfo.getIdentifier())) {
+                    checkMarkImageViews.get(2).setVisibility(View.VISIBLE);
+                }
+            }
+        }
+    }
+
+    /**
+     * TrackingMenuFragmentViewModel contains data that should persist across the fragment life cycle state changes
+     */
+    public static class TrackingMenuFragmentViewModel extends ViewModel {
+        int selectedId = R.id.tracking_tab;
+        void setSelectedId(int selectedId) {
+            this.selectedId = selectedId;
+        }
+        int getCurrentSelectedId() {
+            return selectedId;
+        }
     }
 }
