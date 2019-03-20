a707d0788ae4e588563d5a848236c70519aa965f
Joshua Liu <liujoshua@users.noreply.github.com>
2019-03-20 15:12:59 -0700

    Merge from master and update build tools

--- app/src/main/java/org/sagebionetworks/research/mpower/inject/MPowerApplicationComponent.java
+++ app/src/main/java/org/sagebionetworks/research/mpower/inject/MPowerApplicationComponent.java
@@ -6,7 +6,6 @@ import org.sagebionetworks.bridge.android.di.BridgeApplicationScope;
 import org.sagebionetworks.research.domain.inject.TaskModule;
 import org.sagebionetworks.research.mobile_ui.inject.PerformTaskModule;
 import org.sagebionetworks.research.mpower.MPowerApplication;
-import org.sagebionetworks.research.mpower.measuring.MedicationTimingStepModule;
 import org.sagebionetworks.research.mpower.researchstack.inject.MPowerResearchStackModule;
 import org.sagebionetworks.research.sageresearch_app_sdk.inject.SageResearchAppSDKModule;
 
@@ -19,8 +18,7 @@ import dagger.android.support.AndroidSupportInjectionModule;
 @Component(modules = {PerformTaskModule.class, SageResearchAppSDKModule.class, TaskModule.class,
         MPowerResearchStackModule.class, AppDataModule.class, AndroidSupportInjectionModule.class,
         MPowerSageResearchModule.class, MPowerAppModule.class, MPowerUserModule.class,
-        TrackingStepModule.class, TrackingFragmentsModule.class,
-        MedicationTimingStepModule.class},
+        TrackingStepModule.class, TrackingFragmentsModule.class},
         dependencies = {MPowerUserScopeComponent.class})
 public interface MPowerApplicationComponent extends AndroidInjector<MPowerApplication> {
 
